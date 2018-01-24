aa700af9695bc7555b8b25c959d217de10afefcb
Sudiukil <sudiukil@gmx.fr>
2018-01-24 15:28:49 +0100

    Fixed links

--- webview.js
+++ webview.js
@@ -2,33 +2,44 @@ const path = require('path');
 
 module.exports = (Franz) => {
   function cleanPathname(pathname) {
-    if(pathname.substr(-1) === '/') return str.substr(0, str.length-1);
+    if(pathname.substr(-1) === '/') return pathname.substr(0, pathname.length-1);
+    return pathname;
   }
 
   const handleLinks = function handleLinks() {
     const whitelist = [
+      "calendar.google.com",
       "keep.google.com",
       "drive.google.com",
       "myaccount.google.com",
       "mail.google.com",
       "inbox.google.com",
+      "hangouts.google.com",
       "photos.google.com",
       "plus.google.com",
       "news.google.fr",
-      "hangouts.google.com",
+      "contacts.google.com",
       "/calendar",
+      "/keep",
+      "/drive",
+      "/myaccount",
+      "/mail",
+      "/inbox",
+      "/hangouts",
+      "/photos",
+      "/plus",
+      "/news",
       "/contacts",
     ];
-    const elems = document.querySelectorAll('a');
 
-    elems.forEach(function(e) {
+    document.querySelectorAll('a').forEach(function(link) {
       if(whitelist.includes(link.host) || whitelist.includes(cleanPathname(link.pathname))) {
-        e.onclick = function() {
-          location.href=this.href;
+        link.onclick = function() {
+          location.href = this.href;
           return false;
         }
       }
-    })
+    });
   }
 
   Franz.loop(handleLinks);
